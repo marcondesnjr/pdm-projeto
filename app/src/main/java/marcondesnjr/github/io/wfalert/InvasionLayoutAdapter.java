265c71b59e81341028b5343f61cd83f087584260
José Marcondes do Nascimento Junior <junior3917@gmail.com>
2018-05-24 23:57:53 -0300

    Adicinado exemplo de exibição de detalher em invasions

--- app/src/main/java/marcondesnjr/github/io/wfalert/InvasionLayoutAdapter.java
+++ app/src/main/java/marcondesnjr/github/io/wfalert/InvasionLayoutAdapter.java
@@ -3,10 +3,13 @@ package marcondesnjr.github.io.wfalert;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.LayoutDirection;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import java.util.List;
@@ -37,6 +40,31 @@ public class InvasionLayoutAdapter extends RecyclerView.Adapter<InvasionLayoutAd
         holder.textViewNode.setText(invasion.getMission().getNome());
         holder.textViewFaction.setText(invasion.getMission().getTipo());
         holder.progressBar.setProgress(invasion.getProgress());
+
+        TableLayout tbl = holder.tableLayout;
+        tbl.removeAllViews();
+        //table rewards
+        TableRow tableRow = new TableRow(tbl.getContext());
+        TableLayout.LayoutParams lp = new TableLayout.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT,TableLayout.LayoutParams.WRAP_CONTENT);
+
+        TextView tvAt = new TextView(tableRow.getContext());
+        tvAt.setText(invasion.getAttReward().name());
+        TextView tvAtQt = new TextView(tableRow.getContext());
+        tvAtQt.setText(invasion.getAttRewardQt()+"");
+
+        TextView tvDef = new TextView(tableRow.getContext());
+        tvAt.setText(invasion.getDefReward().name());
+        TextView tvDefQt = new TextView(tableRow.getContext());
+        tvAtQt.setText(invasion.getDefRewardQt()+"");
+
+        tableRow.addView(tvAt);
+        tableRow.addView(tvAtQt);
+        tableRow.addView(tvDef);
+        tableRow.addView(tvDefQt);
+
+        tbl.addView(tableRow,lp);
+
+
     }
 
     @Override
@@ -49,13 +77,27 @@ public class InvasionLayoutAdapter extends RecyclerView.Adapter<InvasionLayoutAd
         private TextView textViewNode;
         private TextView textViewFaction;
         private ProgressBar progressBar;
-        private  TextView textViewTime;
+        private TableLayout tableLayout;
 
-        public InvasionLayoutHolder(View itemView) {
+        public InvasionLayoutHolder(final View itemView) {
             super(itemView);
             this.textViewNode = itemView.findViewById(R.id.textViewNode);
             this.textViewFaction = itemView.findViewById(R.id.textViewMissionFaction);
             this.progressBar = itemView.findViewById(R.id.progressBar);
+            this.tableLayout = itemView.findViewById(R.id.invasionTableReward);
+
+            itemView.findViewById(R.id.invasionCardView).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    TableLayout tbl = v.findViewById(R.id.invasionTableReward);
+                    if(tbl.getVisibility() == View.GONE){
+                        tbl.setVisibility(View.VISIBLE);
+                    }else{
+                        tbl.setVisibility(View.GONE);
+                    }
+                }
+            });
+
         }
     }
 
